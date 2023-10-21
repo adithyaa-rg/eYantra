499709b7240246b549f3efffdead9bcb1b3b35bc
adithyaa <adithyaa2003@gmail.com>
2023-10-21 14:21:24 +0530

    Controller Added

--- Task_2/hb_task_2_ws/src/eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/controller.py
+++ Task_2/hb_task_2_ws/src/eYRC-2023_Hologlyph_Bots/hb_task2a/scripts/controller.py
@@ -32,10 +32,13 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+import numpy as np
 import time
 import math
 from tf_transformations import euler_from_quaternion
 from my_robot_interfaces.srv import NextGoal             
+from geometry_msgs.msg  import Pose2D
+from geometry_msgs.msg import Wrench
 
 # You can add more if required
 ##############################################################
@@ -53,7 +56,7 @@ from my_robot_interfaces.srv import NextGoal
 class HBController(Node):
     def __init__(self):
         super().__init__('hb_controller')
-        
+        self.goal = {}
         # Initialze Publisher and Subscriber
         # NOTE: You are strictly NOT-ALLOWED to use "cmd_vel" or "odom" topics in this task
 	    #	Use the below given topics to generate motion for the robot.
@@ -73,14 +76,61 @@ class HBController(Node):
         self.req = NextGoal.Request() 
         self.index = 0
 
+        pose_subscriber = self.create_subscription(Pose2D,'/detected_aruco',self.find_velocity,10)
+        pose_subscriber  # prevent unused variable warning
+
+        self.wheel_left = self.create_publisher(Wrench, '/hb_bot_1/left_wheel_force', 10)
+        self.wheel_right = self.create_publisher(Wrench, '/hb_bot_1/right_wheel_force', 10)
+        self.wheel_rear = self.create_publisher(Wrench, '/hb_bot_1/rear_wheel_force', 10)
+
+    def send_request(self, index):
+
+        """
+        Sends the request to the "next_goal" service
+
+        Parameters:
+            index: Index of the goal pose in the list of goal poses
+
+        Returns:
+            None
+
+        """
+
+        self.req.request_goal = index
+        self.future = self.cli.call_async(self.req)
+        # self.get_logger().info(f"Request sent: {self.req.request_goal}")
+        rclpy.spin_until_future_complete(self, self.future)
+        # self.get_logger().info(f"Futrue complete")
+
+        return self.future.result()
     
+    def find_velocity(self,msg):
+        x = msg.x
+        y = msg.y
+        theta = msg.theta
+
+        self.kP_linear = 0.5
+        self.kP_angular = 0.5
+
+        self.velocity_x = self.kP_linear*(self.goal['x_goal'] - x)
+        self.velocity_y = self.kP_linear*(self.goal['y_goal'] - y)
+        self.velocity_theta = self.kP_angular*(self.goal['theta_goal'] - theta)
+
+        self.alpha_1 = 30/180*np.pi
+        self.alpha_2 = 150/180*np.pi
+        self.alpha_3 = 270/180*np.pi
+
+        self.global_velocity = np.array([self.velocity_x, self.velocity_y, self.velocity_theta])
+
+        self.inverse_kinematics()
+
     # Method to create a request to the "next_goal" service
     def send_request(self, request_goal):
         self.req.request_goal = request_goal
         self.future = self.cli.call_async(self.req)
         
 
-    def inverse_kinematics():
+    def inverse_kinematics(self):
         ############ ADD YOUR CODE HERE ############
 
         # INSTRUCTIONS & HELP : 
@@ -88,7 +138,28 @@ class HBController(Node):
         #	Process it further to find what proportions of that effort should be given to 3 individuals wheels !!
         #	Publish the calculated efforts to actuate robot by applying force vectors on provided topics
         ############################################
-        pass
+
+        component_matrix = [[np.cos(self.alpha_1 + np.pi/2), np.cos(self.alpha_2 + np.pi/2), np.cos(self.alpha_3 + np.pi/2)],
+                            [np.sin(self.alpha_1 + np.pi/2), np.sin(self.alpha_2 + np.pi/2), np.sin(self.alpha_3 + np.pi/2)],
+                            [1, 1, 1]]
+        component_matrix = np.array(component_matrix)
+
+        component_inverse = np.linalg.inv(component_matrix)
+
+        wheel_velocity = np.matmul(component_inverse, self.global_velocity)
+
+        wheel_1 = Wrench()
+        wheel_1.force.y = wheel_velocity[0]
+        wheel_2 = Wrench()
+        wheel_2.force.y = wheel_velocity[1]
+        wheel_3 = Wrench()
+        wheel_3.force.y = wheel_velocity[2]
+
+        self.wheel_right.publish(wheel_1)
+        self.wheel_left.publish(wheel_2)
+        self.wheel_rear.publish(wheel_3)
+
+
 
 
 def main(args=None):
@@ -113,9 +184,9 @@ def main(args=None):
                     'Service call failed %r' % (e,))
             else:
                 #########           GOAL POSE             #########
-                x_goal      = response.x_goal
-                y_goal      = response.y_goal
-                theta_goal  = response.theta_goal
+                hb_controller.goal['x_goal']      = response.x_goal
+                hb_controller.goal['y_goal']      = response.y_goal
+                hb_controller.goal['theta_goal']  = response.theta_goal
                 hb_controller.flag = response.end_of_list
                 ####################################################
                 
