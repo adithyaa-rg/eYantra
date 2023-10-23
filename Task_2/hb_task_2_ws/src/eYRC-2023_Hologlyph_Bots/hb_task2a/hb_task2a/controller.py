fced784846c4bb8bc7e103cd7bd4ab5a445d08cb
adithyaa <adithyaa2003@gmail.com>
2023-10-24 00:51:35 +0530

    controller changes

--- Task_2/hb_task_2_ws/src/eYRC-2023_Hologlyph_Bots/hb_task2a/hb_task2a/controller.py
+++ Task_2/hb_task_2_ws/src/eYRC-2023_Hologlyph_Bots/hb_task2a/hb_task2a/controller.py
@@ -56,7 +56,11 @@ from geometry_msgs.msg import Wrench
 class HBController(Node):
     def __init__(self):
         super().__init__('hb_controller')
-        self.goal = {}
+        self.goal = {
+            'x_goal': 100,
+            'y_goal': 100,
+            'theta_goal': 2
+        }
         # Initialze Publisher and Subscriber
         # NOTE: You are strictly NOT-ALLOWED to use "cmd_vel" or "odom" topics in this task
 	    #	Use the below given topics to generate motion for the robot.
@@ -64,7 +68,9 @@ class HBController(Node):
 	    #   /hb_bot_1/right_wheel_force,
 	    #   /hb_bot_1/left_wheel_force
 
-
+        self.goal['x_goal'] = self.goal['x_goal']
+        self.goal['y_goal'] = self.goal['y_goal']
+        self.goal['theta_goal'] = self.goal['theta_goal']
 
 
         # For maintaining control loop rate.
@@ -109,12 +115,28 @@ class HBController(Node):
         y = msg.y
         theta = msg.theta
 
-        self.kP_linear = 0.5
-        self.kP_angular = 0.5
+        self.kP_linear = 0.1
+        self.kP_angular = 0.2
 
-        self.velocity_x = self.kP_linear*(self.goal['x_goal'] - x)
-        self.velocity_y = self.kP_linear*(self.goal['y_goal'] - y)
+        global_pos_error_x = self.goal['x_goal'] - x
+        global_pos_error_y = self.goal['y_goal'] - y
+
+        self.velocity_x = self.kP_linear*(global_pos_error_x*np.cos(theta) + global_pos_error_y*np.sin(theta))
+        self.velocity_y = self.kP_linear*(-global_pos_error_x*np.sin(theta) + global_pos_error_y*np.cos(theta))
         self.velocity_theta = self.kP_angular*(self.goal['theta_goal'] - theta)
+
+        print(f''' Goal:
+{self.goal['x_goal']}
+{self.goal['y_goal']}
+{self.goal['theta_goal']}''')
+        print(f''' Current:
+{x}
+{y}
+{theta}''')
+        print(f''' Error:
+{self.velocity_x}
+{self.velocity_y}
+{self.velocity_theta}''')
 
         self.alpha_1 = 30/180*np.pi
         self.alpha_2 = 150/180*np.pi
@@ -169,44 +191,44 @@ def main(args=None):
     hb_controller = HBController()
    
     # Send an initial request with the index from ebot_controller.index
-    hb_controller.send_request(hb_controller.index)
+    # hb_controller.send_request(hb_controller.index)
     
     # Main loop
     while rclpy.ok():
 
         # Check if the service call is done
-        if hb_controller.future.done():
-            try:
-                # response from the service call
-                response = hb_controller.future.result()
-            except Exception as e:
-                hb_controller.get_logger().infselfo(
-                    'Service call failed %r' % (e,))
-            else:
-                #########           GOAL POSE             #########
-                hb_controller.goal['x_goal']      = response.x_goal
-                hb_controller.goal['y_goal']      = response.y_goal
-                hb_controller.goal['theta_goal']  = response.theta_goal
-                hb_controller.flag = response.end_of_list
-                ####################################################
+        # if hb_controller.future.done():
+        #     try:
+        #         # response from the service call
+        #         response = hb_controller.future.result()
+        #     except Exception as e:
+        #         hb_controller.get_logger().infselfo(
+        #             'Service call failed %r' % (e,))
+        #     else:
+        #         #########           GOAL POSE             #########
+        #         hb_controller.goal['x_goal']      = response.x_goal
+        #         hb_controller.goal['y_goal']      = response.y_goal
+        #         hb_controller.goal['theta_goal']  = response.theta_goal
+        #         hb_controller.flag = response.end_of_list
+        #         ####################################################
                 
-                # Calculate Error from feedback
+        #         # Calculate Error from feedback
 
-                # Change the frame by using Rotation Matrix (If you find it required)
+        #         # Change the frame by using Rotation Matrix (If you find it required)
 
-                # Calculate the required velocity of bot for the next iteration(s)
+        #         # Calculate the required velocity of bot for the next iteration(s)
                 
-                # Find the required force vectors for individual wheels from it.(Inverse Kinematics)
+        #         # Find the required force vectors for individual wheels from it.(Inverse Kinematics)
 
-                # Apply appropriate force vectors
+        #         # Apply appropriate force vectors
 
-                # Modify the condition to Switch to Next goal (given position in pixels instead of meters)
+        #         # Modify the condition to Switch to Next goal (given position in pixels instead of meters)
                         
-                ############     DO NOT MODIFY THIS       #########
-                hb_controller.index += 1
-                if hb_controller.flag == 1 :
-                    hb_controller.index = 0
-                hb_controller.send_request(hb_controller.index)
+        #         ############     DO NOT MODIFY THIS       #########
+        #         hb_controller.index += 1
+        #         if hb_controller.flag == 1 :
+        #             hb_controller.index = 0
+        #         hb_controller.send_request(hb_controller.index)
                 ####################################################
 
         # Spin once to process callbacks
